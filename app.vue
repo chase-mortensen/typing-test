f28fe13e550c61b99d58da3b009e357e479936dd
Chase Mortensen <chase@chase-mortensen.dev>
2023-08-18 21:05:26 -0600

    Starting to change to NES.css and options API

--- app.vue
+++ app.vue
@@ -1,12 +1,18 @@
 <template>
+  <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet" />
+  <link href="https://unpkg.com/nes.css/css/nes.css" rel="stylesheet" />
   <NuxtLayout>
     <Home />
   </NuxtLayout>
 </template>
 
 <script setup>
+
+
 useHead({
     title: 'Typing Test',
-    link: {rel:"stylesheet", href:"https://rsms.me/inter/inter.css"}
+    link: {rel:"stylesheet", href:"https://rsms.me/inter/inter.css"},
+    link: {rel:"stylesheet", href:"https://fonts.googleapis.com/css?family=Press+Start+2P"},
+    link: {rel:"stylesheet", href:"https://unpkg.com/nes.css/css/nes.css"}
   })
 </script>
--- components/Home.vue
+++ components/Home.vue
@@ -5,39 +5,57 @@
     class="pb-4"
     description="Test Duration"
     :options="durations"
-    @setOption="(d) => state.testLength = d"
-    v-show="state.timeLeft <= 0"
+    @setOption="(d) => testLength = d"
+    v-show="timeLeft <= 0"
   />
-
-  <!-- <p class="pb-4">Test duration: {{ durationString }}</p> -->
-
   <TypeTest />
-
-  <!-- <p class="pb-4">{{ currentString }} {{ currentString.length }}</p> -->
-
 </template>
 
 <script setup>
-import { reactive } from 'vue'
+// import { reactive } from 'vue'
 
-const state = reactive({
-  testLength: 5,
-  timeLeft: 0
- })
-const durations = [
-  {id: 30, name: '30 seconds'},
-  {id: 5, name: '5 seconds'},
-  {id: 60, name: '60 seconds'},
-  {id: 90, name: '90 seconds'},
-  {id: 120, name: '2 minutes'},
-  {id: 180, name: '3 minutes'},
-]
+// const state = reactive({
+//   testLength: 5,
+//   timeLeft: 0
+//  })
+// const durations = [
+//   {id: 30, name: '30 seconds'},
+//   {id: 5, name: '5 seconds'},
+//   {id: 60, name: '60 seconds'},
+//   {id: 90, name: '90 seconds'},
+//   {id: 120, name: '2 minutes'},
+//   {id: 180, name: '3 minutes'},
+// ]
 
-const currentString = getWordStrMaxLen(100)
+// const currentString = getWordStrMaxLen(100)
 
-const durationString = computed(() => {
-  return durations.find(d => d.id === state.testLength)?.name
-})
+// const durationString = computed(() => {
+//   return durations.find(d => d.id === state.testLength)?.name
+// })
+</script>
+
+<script>
+  export default {
+    data () {
+      return {
+        testLength: 5,
+        timeLeft: 0,
+        durations: [
+          {id: 30, name: '30 seconds'},
+          {id: 5, name: '5 seconds'},
+          {id: 60, name: '60 seconds'},
+          {id: 90, name: '90 seconds'},
+          {id: 120, name: '2 minutes'},
+          {id: 180, name: '3 minutes'},
+      ]
+      }
+    },
+    computed: {
+      durationString () {
+        return this.durations.find(d => d.id === this.testLength)?.name
+      }
+    }
+  }
 </script>
 
 <style>
