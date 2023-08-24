b15b29d590952e5b77d40546aae78f6535b837ea
Chase Mortensen <chase@chase-mortensen.dev>
2023-08-24 00:16:33 -0600

    Minor styling fixes - NES.css

--- components/StatBox.vue
+++ components/StatBox.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="overflow-hidden rounded-lg bg-white mb-5 px-4 py-5 shadow sm:p-6">
-    <dt class="truncate text-sm font-medium text-gray-500">{{ item?.name }}</dt>
+  <div class="nes-container overflow-hidden bg-white mb-5 px-4 py-5 shadow sm:p-6">
+    <dt class="truncate text-xs font-medium text-gray-500">{{ item?.name }}</dt>
     <dd class="mt-1 text-xl font-semibold tracking-tight text-gray-900">{{ item?.stat }}</dd>
   </div>
 </template>
@@ -9,6 +9,8 @@
   const props = defineProps(['item'])
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  .nes-container {
+    cursor: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABFklEQVRYR9WXURLDIAhE6/0PbSdOtUpcd1Gnpv1KGpTHBpCE1/cXq+vrMph7dGvXZTtpfW10DCA5jrH1H0Jhs5E0hnZdCR+vb5S8Nn8mQCeS9BdSalYJqMBjAGzq59xAESN7VFVUgV8AZB/dZBR7QTFDCqGquvUBVVoEtgIwpQRzmANSFHgWQKExHdIrPeuMvQNDarXe6nC/AutgV3JW+6bgqQLeV8FekRtgV+ToDKEKnACYKsfZjjkam7a0ZpYTytwmgainpC3HvwBocgKOxqRjehoR9DFKNFYtOwCGYCszobeCbl26N6yyQ6g8X/Wex/rBPsNEV6qAMaJPMynIHQCoSqS9JSMmwef51LflTgCRszU7DvAGiV6mHWfsaVUAAAAASUVORK5CYII=), auto;
+  }
 </style>
